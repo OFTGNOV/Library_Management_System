0f215e016b5f5644ce39c1160912cd7462ce32a1
Tamai Richards <122725333+OFTGNOV@users.noreply.github.com>
2025-03-08 19:31:00 -0500

    Edited The information in #Files Go Here.h

--- Library Management System/Files Go here.h
+++ Library Management System/Files Go here.h
@@ -1,2 +1,2 @@
-//So This File Was basically Created So you know where all the other files Should be
+//So This File Was basically Created So you know where all the other files Should be Header Files, Source Files, etc should be placed in the Library Management System Folder
 
